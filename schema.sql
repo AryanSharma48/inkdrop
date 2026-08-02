0c765b29ed2ef183bad91a69a16a100b39b8d019
Aryan Sharma <aryansharma24106@gmail.com>
2026-08-03 01:47:18 +0530

    db: update schema with users table and missing paste columns

--- schema.sql
+++ schema.sql
@@ -1,8 +1,19 @@
 CREATE TABLE IF NOT EXISTS pastes (
   id TEXT PRIMARY KEY,
+  userId TEXT,
   title TEXT,
   language TEXT,
   visibility TEXT NOT NULL,
   text TEXT NOT NULL,
-  expiresAt INTEGER
+  isBurn INTEGER,
+  expiresAt INTEGER,
+  passwordHash TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id TEXT PRIMARY KEY,
+  githubId TEXT NOT NULL,
+  userName TEXT NOT NULL,
+  avatarUrl TEXT NOT NULL,
+  createdAt INTEGER
 );
